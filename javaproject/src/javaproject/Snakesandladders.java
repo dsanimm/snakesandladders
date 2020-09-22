58d4209421b8488a6f598364af1d46befcb2a239
dsanimm <dsanimm@gmail.com>
2020-09-22 20:32:34 +0530

    prob2testcase1

--- javaproject/src/javaproject/Snakesandladders.java
+++ javaproject/src/javaproject/Snakesandladders.java
@@ -1,7 +1,17 @@
-package javaproject;
+ackage javaproject;
 
-public class Snakesandladders{
-public static void main(String[] args) {
-	System.out.println("Hello World");
-}
+public class Snakesandladders {
+public static void main(String args []) {
+
+	int pos1 = 0;
+	int pos2 = 0;
+
+	System.out.println("Player 1");
+	System.out.println("Player 1 position: "+pos1);
+
+	System.out.println("Player 2");
+	System.out.println("Player 2 position: "+pos2);
+
+
+} 
 }
