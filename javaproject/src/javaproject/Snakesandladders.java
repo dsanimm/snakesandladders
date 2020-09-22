cc91686fcb469c5ed72c9f7f859716b6ece35d84
dsanimm <dsanimm@gmail.com>
2020-09-22 20:34:32 +0530

    prob2testcase2

--- javaproject/src/javaproject/Snakesandladders.java
+++ javaproject/src/javaproject/Snakesandladders.java
@@ -1,17 +1,19 @@
-ackage javaproject;
-
-public class Snakesandladders {
-public static void main(String args []) {
-
-	int pos1 = 0;
-	int pos2 = 0;
-
+package javaproject;
+public class helloworld {
+	public static void main(String args []) {
+		
+		int pos1 = 0;
+		int pos2 = 0;
 	System.out.println("Player 1");
 	System.out.println("Player 1 position: "+pos1);
+	int die1 = ((int) (6*Math.random()))+1;
 
+	System.out.println("Player 1");
+	System.out.println("die: "+die1);
 	System.out.println("Player 2");
 	System.out.println("Player 2 position: "+pos2);
-
-
-} 
+	int die2 = ((int) (6*Math.random()))+1;
+	System.out.println("Player 2");
+	System.out.println("die: "+die2);
 }
+	} 
